4971e1852d0c695027f5b622c71b55ee349493ae
Haoz <developerhaon@gmail.com>
2017-10-03 22:56:47 +0800

    性能优化的一些小 Demo

---
+++ app/src/main/java/com/developerhaoz/performanceoptimizationdemo/OverdrawActivity.java
@@ -0,0 +1,18 @@
+package com.developerhaoz.performanceoptimizationdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by developerHaoz on 2017/10/3.
+ */
+
+public class OverdrawActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overdraw);
+    }
+}
