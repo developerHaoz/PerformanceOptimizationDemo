c8a97e29c324438311f59d86911d83fbc343d500
Haoz <developerhaon@gmail.com>
2017-10-09 23:37:45 +0800

    一些小修改

--- app/src/main/java/com/developerhaoz/performanceoptimizationdemo/MemoryLeakActivity.java
+++ app/src/main/java/com/developerhaoz/performanceoptimizationdemo/MemoryLeakActivity.java
@@ -3,6 +3,7 @@ package com.developerhaoz.performanceoptimizationdemo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 /**
  * Created by developerHaoz on 2017/10/3.
@@ -10,9 +11,12 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MemoryLeakActivity extends AppCompatActivity {
 
+    private static final String TAG = "MemoryLeakActivity";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memory_leak);
+        Log.d(TAG, "onCreate: " + "不查不知道，一查吓一跳");
     }
 }
--- app/src/main/java/com/developerhaoz/performanceoptimizationdemo/OverdrawActivity.java
+++ app/src/main/java/com/developerhaoz/performanceoptimizationdemo/OverdrawActivity.java
@@ -3,6 +3,7 @@ package com.developerhaoz.performanceoptimizationdemo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 /**
  * Created by developerHaoz on 2017/10/3.
@@ -10,9 +11,12 @@ import android.support.v7.app.AppCompatActivity;
 
 public class OverdrawActivity extends AppCompatActivity {
 
+    private static final String TAG = "OverdrawActivity";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overdraw);
+        Log.d(TAG, "onCreate: " + "这个只能说尽量减少布局嵌套吧");
     }
 }
